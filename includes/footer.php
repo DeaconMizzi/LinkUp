1840ec2c1dbc69e2cfd6a11da4cdfcae0d83bb91
Deacon <deaconmizzi@gmail.com>
2024-06-04 20:01:26 +0200

    Final Post HTML Page
    Addition of icons, and final css

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="footer-container">
+        <p>&copy; 2024 LinkUp. All rights reserved.</p>
+        <div class="footer-nav">
+            <a href="/linkup/templates/pages/faq.php">FAQ</a>
+            <a href="/linkup/templates/pages/contact.php">Contact Us</a>
+        </div>
+        <div class="footer-social">
+        </div>
+    </div>
+</footer>
+</body>
+</html>
+
